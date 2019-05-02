ed59e302fdcc94b26a41c25e90fedd25e2075ed2
DamSaulGoodMan <capron.damien@laposte.net>
2019-05-02 17:14:26 +0200

    added icon and pagination !

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,15 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Sidebar.css';
-
+import arrow_right from "./arrow_right.png";
+import arrow_left from "./arrow_left.png";
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <picture />
-            <a href={"#home"}>Home</a>
-            <a href={"#news"}>News</a>
-            <a href={"#contact"}>Contact</a>
-            <a href={"#about"}>About</a>
+            <img src={arrow_left} alt={"<<"}/>
+            <ul>
+                <li>
+                    <a href={"#home"}>Home</a>
+                </li>
+                <li>
+                    <a href={"#profile"}>Profile</a>
+                </li>
+                <li>
+                    <a href={"#friends"}>Friends</a>
+                </li>
+                <li>
+                    <a href={"#enigmas"}>Enigmas</a>
+                </li>
+                <li>
+                    <a href={"#create_enigma"}>Create</a>
+                </li>
+                <li>
+                    <a href={"#rank"}>Rank</a>
+                </li>
+                <li>
+                    <a href={"#forum"}>Forum</a>
+                </li>
+                <li>
+                    <a href={"#rgpd"}>RGPD</a>
+                </li>
+                <li>
+                    <a href={"#settings"}>Settings</a>
+                </li>
+            </ul>
         </div>
     );
 }

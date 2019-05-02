65a53888c08a4530fdbd6268ad9da35e879e6658
DamSaulGoodMan <capron.damien@laposte.net>
2019-05-03 00:17:09 +0200

    try to use FontAwesome Icon ^^

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -3,13 +3,21 @@ import './Sidebar.css';
 import arrow_right from "./arrow_right.png";
 import arrow_left from "./arrow_left.png";
 
+//https://codepen.io/CybMeta/pen/yyOLVz
+//https://codepen.io/rusaidmrd/pen/xdEZya
+//https://fontawesome.com/icons/
+
 function Sidebar() {
     return (
         <div className="sidebar">
+            <script defer src="https://use.fontawesome.com/releases/v5.8.1/js/all.js"
+                    integrity="sha384-g5uSoOSBd7KkhAMlnQILrecXvzst9TdC09/VM+pjDTCM+1il8RHz5fKANTFFb+gQ"
+                    crossOrigin="anonymous">
+            </script>
             <img src={arrow_left} alt={"<<"}/>
             <ul>
                 <li>
-                    <a href={"#home"}>Home</a>
+                    <a href={"#home"}><i className={"far fa-address-card"}/>Home</a>
                 </li>
                 <li>
                     <a href={"#profile"}>Profile</a>

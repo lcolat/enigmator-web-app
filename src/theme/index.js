5aa0b46eeb63347a08e78128a3a71ee00689e663
Loïc Colat <loic.colat@gmail.com>
2019-05-08 12:28:43 +0200

    :construction::lipstick: add theme

---
+++ src/theme/index.js
@@ -0,0 +1,16 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export default createMuiTheme({
+	typography: {
+		useNextVariants: true,
+		fontFamily: ['Nunito'].join(',')
+	},
+	palette: {
+		primary: {
+			main: '#ae75e9'
+		},
+		secondary: {
+			main: '#ffffff'
+		}
+	}
+})

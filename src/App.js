793261ab38f9e79f01fbd809e9a8cd87b8783ad8
Loïc Colat <loic.colat@gmail.com>
2019-05-21 18:38:27 +0200

    :construction: add privateRoute

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import { MuiThemeProvider } from '@material-ui/core/styles'
+import { Route, Switch } from 'react-router-dom'
 import Header from './components/header'
-import Authentication from './components/authentication'
+import Authentication from './components/login/authentication'
+import ForgottentPassword from './components/login/forgottenPassword'
+import NewAccount from './components/login/newAccount'
 import './App.css';
 import theme from './theme'
+import PrivateRoute from './privateRoute';
 
 
 class App extends Component {
@@ -12,7 +16,12 @@ class App extends Component {
       <MuiThemeProvider theme={theme}>      
         <div className="App">
           <Header />
-          <Authentication/>
+          <Switch>
+            <Route path="/login" component={Authentication} />
+            <Route path="/forgotten-password" component={ForgottentPassword} />
+            <Route path="/new-account" component={NewAccount} />
+            <PrivateRoute path="/" />
+          </Switch>
         </div> 
       </MuiThemeProvider>
 

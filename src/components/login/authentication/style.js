09d6e04f9d9c35e51268974a498d3135f7de9b25
Loïc Colat <loic.colat@gmail.com>
2019-05-31 16:03:56 +0200

    :lipstick: new login style

--- src/components/login/authentication/style.js
+++ src/components/login/authentication/style.js
@@ -1,7 +1,23 @@
-const style = () => ({
+const style = theme => ({
+	content: {
+		marginTop: '20vh'
+	},
+	email: {
+		minWidth: '250px'
+	},
+	password: {
+		minWidth: '250px'
+	},
 	button: {
+		minWidth: '125px',
 		marginTop: '1.5vh',
 		marginBottom: '1.5vh'
+	},
+	enigmatorLogo: {
+		[theme.breakpoints.down('sm')]: {
+			height: '15vh'
+		},
+		height: '20vh'
 	}
 })
 

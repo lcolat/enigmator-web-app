e32bf8d784c899f77f424d18b0793563aaa8086d
Damien Capron <capron.damien@laposte.net>
2019-06-06 00:39:14 +0200

    add STACK, cause there is NO stack in JAVASCRIPT MES COUILLES

--- src/components/enigmas-list/enigmas-play-ground/EnigmaTemplate.js
+++ src/components/enigmas-list/enigmas-play-ground/EnigmaTemplate.js
@@ -35,78 +35,80 @@ const useStyles = makeStyles(theme => ({
 // };
 
 
-function InfoEnigma(props) {
-	
-	const {enigma} = props;
+// function InfoEnigma(props) {
+//
+// 	const {enigma} = props;
+// 	const classes = useStyles();
+//
+// 	return (
+// 		<Paper className={classes.rootEnigmaInfo}>
+// 			<List
+// 				component="nav"
+// 				subheader={<ListSubheader component="div">Enigma's Specificity</ListSubheader>}
+// 				className={classes.rootEnigmaInfo}
+// 			>
+// 				<Divider/>
+// 				<Tooltip title={"Creator Name"} placement={"right"}>
+// 					<ListItem alignItems={"center"}>
+// 						<ListItemIcon>
+// 							<EventSeat/>
+// 						</ListItemIcon>
+// 						<ListItemText primary={enigma.creator}/>
+// 					</ListItem>
+// 				</Tooltip>
+// 				<Divider/>
+// 				<Tooltip title={"Creation Date"} placement={"right"}>
+// 					<ListItem alignItems={"center"}>
+// 						<ListItemIcon>
+// 							<Create/>
+// 						</ListItemIcon>
+// 						<ListItemText primary={enigma.date}/>
+// 					</ListItem>
+// 				</Tooltip>
+// 				<Divider/>
+// 				<Tooltip title={"Level of Difficulty"} placement={"right"}>
+// 					<ListItem alignItems={"center"}>
+// 						<ListItemIcon>
+// 							<FitnessCenter/>
+// 						</ListItemIcon>
+// 						<ListItemText primary={enigma.difficulty}/>
+// 					</ListItem>
+// 				</Tooltip>
+// 				<Divider/>
+// 				<Tooltip title={"Reward Points"} placement={"right"}>
+// 					<ListItem alignItems={"center"}>
+// 						<ListItemIcon>
+// 							<LocalActivity/>
+// 						</ListItemIcon>
+// 						<ListItemText primary={enigma.value}/>
+// 					</ListItem>
+// 				</Tooltip>
+// 			</List>
+// 		</Paper>
+// 	);
+// }
+//
+// export default InfoEnigma({
+// 	enigma: {
+// 		creator: "DamSaulGoodMan",
+// 		date: "18:02, 18/08/2018", difficulty: "HARD",
+// 		value: "75"
+// 	}
+// });
+
+
+export default function ListWordTry() {
 	const classes = useStyles();
 	
 	return (
-		<Paper className={classes.rootEnigmaInfo}>
-			<List
-				component="nav"
-				subheader={<ListSubheader component="div">Enigma's Specificity</ListSubheader>}
-				className={classes.rootEnigmaInfo}
-			>
-				<Divider/>
-				<Tooltip title={"Creator Name"} placement={"right"}>
-					<ListItem alignItems={"center"}>
-						<ListItemIcon>
-							<EventSeat/>
-						</ListItemIcon>
-						<ListItemText primary={enigma.creator}/>
+		<div>
+			<List className={classes.rootWordTry}>
+				{[0, 1, 2].map(item => (
+					<ListItem key={`item-${item}`}>
+						<ListItemText primary={`Item ${item}`}/>
 					</ListItem>
-				</Tooltip>
-				<Divider/>
-				<Tooltip title={"Creation Date"} placement={"right"}>
-					<ListItem alignItems={"center"}>
-						<ListItemIcon>
-							<Create/>
-						</ListItemIcon>
-						<ListItemText primary={enigma.date}/>
-					</ListItem>
-				</Tooltip>
-				<Divider/>
-				<Tooltip title={"Level of Difficulty"} placement={"right"}>
-					<ListItem alignItems={"center"}>
-						<ListItemIcon>
-							<FitnessCenter/>
-						</ListItemIcon>
-						<ListItemText primary={enigma.difficulty}/>
-					</ListItem>
-				</Tooltip>
-				<Divider/>
-				<Tooltip title={"Reward Points"} placement={"right"}>
-					<ListItem alignItems={"center"}>
-						<ListItemIcon>
-							<LocalActivity/>
-						</ListItemIcon>
-						<ListItemText primary={enigma.value}/>
-					</ListItem>
-				</Tooltip>
+				))}
 			</List>
-		</Paper>
-	);
-}
-
-export default InfoEnigma({
-	enigma: {
-		creator: "DamSaulGoodMan",
-		date: "18:02, 18/08/2018", difficulty: "HARD",
-		value: "75"
-	}
-});
-
-
-export function ListWordTry() {
-	const classes = useStyles();
-	
-	return (
-		<List className={classes.rootWordTry}>
-			{[0, 1, 2].map(item => (
-				<ListItem key={`item-${item}`}>
-					<ListItemText primary={`Item ${item}`}/>
-				</ListItem>
-			))}
-		</List>
+		</div>
 	);
 }

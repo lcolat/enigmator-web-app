36381bf8e04ddf9f69635c17cac9f1c910fb0018
DamSaulGoodMan <capron.damien@laposte.net>
2019-06-11 16:07:49 +0200

    Thanks to my busy work, i can work on this project :) no more bug with Slider (i don't know why exactly but it doesn't matter :p)

--- src/components/enigmas-list/enigmas-play-ground/VocalEnigma.js
+++ src/components/enigmas-list/enigmas-play-ground/VocalEnigma.js
@@ -6,25 +6,39 @@ import {Grid} from '@material-ui/core';
 
 import {Slider} from "@material-ui/lab";
 
-import {MusicNoteRounded} from "@material-ui/icons";
+import {MusicNoteRounded, VolumeDown, VolumeUp} from "@material-ui/icons";
 
 
 const styles = theme => ({
 	root: {
+		display: 'flex',
+		//width: "80%",
+		//height: 300
+	},
+	rootSliderSound: {
+		//display: 'flex',
 		width: "80%",
-		height: 300
+		//height: 100,
 	},
 	sliderSound: {
 		padding: '22px 0px',
-		margin: theme.spacing.unit,
+		margin: theme.spacing.unit * 0.5,
+		marginRight: 0
 	},
 	rootSliderVolume: {
 		display: 'flex',
-		height: 300,
+		width: "100%",
+		height: 80,
 	},
 	sliderVolume: {
 		padding: '0px 22px',
-		// margin: theme.spacing.unit,
+		width: 0,
+		margin: theme.spacing.unit * 0.5,
+		marginBottom: 0
+	},
+	iconsVolume: {
+		display: 'flex',
+		height: 80
 	}
 });
 
@@ -35,6 +49,7 @@ class VocalEnigma extends React.Component {
 	};
 	
 	render() {
+		const maxVolume = 17;
 		const {classes, soundDuration} = this.props;
 		const {currentVolume, soundAdvancement} = this.state;
 		
@@ -47,40 +62,57 @@ class VocalEnigma extends React.Component {
 		
 		return (
 			<div className={classes.root}>
-				<Grid container>
-					<Slider className={classes.sliderSound}
-					        value={soundAdvancement}
-					        min={0}
-					        max={soundDuration}
-					        thumb={<MusicNoteRounded/>}
-					        onChange={handleChangeSoundAdvancement}/>
-					<Grid item container justify="space-between">
-						<Grid item>
-							{soundAdvancement}
-						</Grid>
-						<Grid item>
-							{soundDuration}
-						</Grid>
-					</Grid>
-					<div>
-						<Slider className={classes.sliderVolume}
-						        vertical
-						        value={currentVolume}
+				<Grid container direction={"column"}>
+					<div className={classes.rootSliderSound}>
+						<Slider className={classes.sliderSound}
+						        value={soundAdvancement}
 						        min={0}
-						        max={17}
-						        onChange={handleChangeVolume}/>
+						        max={soundDuration}
+						        thumb={<MusicNoteRounded/>}
+						        onChange={handleChangeSoundAdvancement}/>
+						<Grid item container justify="space-between">
+							<Grid item>
+								{soundAdvancement}
+							</Grid>
+							<Grid item>
+								{soundDuration}
+							</Grid>
+						</Grid>
 					</div>
-					<Grid item container justify="space-between" direction={"row"}>
-						{currentVolume}
-						{"17"}
-					</Grid>
+					<div className={classes.rootSliderVolume}>
+						<Grid container direction={"row"} justify={"flex-end"}>
+							<Slider className={classes.sliderVolume}
+							        vertical
+							        value={currentVolume}
+							        min={0}
+							        max={maxVolume}
+							        step={1}
+							        onChange={handleChangeVolume}/>
+							<div className={classes.iconsVolume}>
+								<Grid item container justify={"space-between"} direction={"column"}>
+									<Grid item>
+										<VolumeUp/>
+									</Grid>
+									<Grid item>
+										{currentVolume}
+									</Grid>
+									<Grid item>
+										<VolumeDown/>
+									</Grid>
+								</Grid>
+							</div>
+						</Grid>
+					</div>
 				</Grid>
 			</div>
 		);
 	};
 }
 
-VocalEnigma.propTypes = {};
+VocalEnigma.propTypes = {
+	volume: PropType.number.isRequired,
+	soundDuration: PropType.number.isRequired
+};
 
 
 export default withStyles(styles)(VocalEnigma)

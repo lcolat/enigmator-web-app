30c59697403fd2f0d6a73002ef801ec3fff98ae6
Loïc Colat <loic.colat@gmail.com>
2019-06-15 19:20:49 +0200

    :bug: ay ay ay error in merge conflic

--- src/components/create-enigma/enigma-builder/index.js
+++ src/components/create-enigma/enigma-builder/index.js
@@ -1,33 +1,30 @@
-import React from 'react';
-import PropType from "prop-types";
+import React from 'react'
+import PropType from 'prop-types'
 
-import {makeStyles} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core'
 
-import {Type} from "./../../../model/Enigma";
-import Template from "./Template";
-import PictureSelector from "./PictureSelector";
-import TextualField from "./TextualField";
-import VocalSelector from "./VocalSelector";
+import { listEnigmasTypes } from './../../../model/Enigma'
+import Template from './Template'
+import PictureSelector from './PictureSelector'
+import TextualField from './TextualField'
+import VocalSelector from './VocalSelector'
 
-
-const useStyles = makeStyles(theme => ({}));
-
+const useStyles = makeStyles(theme => ({}))
 
 function EnigmaBuilder(props) {
-	const {type} = props;
-	const classes = useStyles();
-	
+	const { type } = props
+	const classes = useStyles()
+
 	switch (type) {
-		case Type.text:
-			return <Template component={<TextualField/>}/>;
-		case Type.photo:
-			return <Template component={<PictureSelector/>}/>;
-		case Type.vocal:
-			return <Template component={<VocalSelector/>}/>
+		case listEnigmasTypes.text:
+			return <Template component={<TextualField />} />
+		case listEnigmasTypes.photo:
+			return <Template component={<PictureSelector />} />
+		case listEnigmasTypes.vocal:
+			return <Template component={<VocalSelector />} />
 	}
 }
 
-EnigmaBuilder.propTypes = {};
+EnigmaBuilder.propTypes = {}
 
-
-export default EnigmaBuilder;
+export default EnigmaBuilder

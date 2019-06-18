f60831d48d78ab51504ab5d7cb3f548bb5456cef
Loïc Colat <loic.colat@gmail.com>
2019-06-18 23:53:31 +0200

    :construction: maybe sudo?

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 PATH="/home/ubuntu/.local/bin:$PATH"
 cd /home/ubuntu/enigmator/enigmator-web-app/
-aws s3 cp s3://enigmator-storage/enigmator-web-app/.env .env
+sudo aws s3 cp s3://enigmator-storage/enigmator-web-app/.env .env
+npm install
 npm run build
 cd /home/ubuntu/enigmator/
 sudo docker-compose restart

b9ecd2ba415131c82cd40ac07bca394a45cb413f
Loïc Colat <loic.colat@gmail.com>
2019-06-18 10:32:35 +0200

    :art: reformat code

--- src/components/friend-page/profile/index.js
+++ src/components/friend-page/profile/index.js
@@ -1,57 +1,63 @@
-import React from "react";
+import React from 'react'
 
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles'
 
-import {Button, Grid, Tooltip, Typography, Paper} from "@material-ui/core";
+import { Button, Grid, Tooltip, Typography, Paper } from '@material-ui/core'
 
-import Avatar from "@material-ui/core/Avatar";
-import {DonutSmall, CompareArrows} from "@material-ui/icons";
+import Avatar from '@material-ui/core/Avatar'
+import { DonutSmall, CompareArrows } from '@material-ui/icons'
 
 import {mapUserStatusColor} from "../../../model/User";
 
 import TableEnigmas from "./TableEnigmas"
 import {StatsTable} from "../../../commun";
 
-
 const styles = theme => ({
 	avatar: {
 		margin: theme.spacing.unit,
 		width: 120,
-		height: 120,
+		height: 120
 	},
 	statusIcon: {
 		margin: theme.spacing.unit,
 		width: 30,
-		height: 30,
+		height: 30
 	},
 	button: {
-		margin: theme.spacing.unit,
+		margin: theme.spacing.unit
 	},
 	rightIcon: {
-		marginLeft: theme.spacing.unit,
+		marginLeft: theme.spacing.unit
 	},
 	buttonRightIcon: {
 		marginLeft: theme.spacing.unit
 	}
-});
+})
 
 class ProfileFriend extends React.Component {
-	
 	render() {
-		const {classes, pseudo, profilePicture, status} = this.props;
-		
+		const { classes, pseudo, profilePicture, status } = this.props
+
 		return (
 			<div>
 				<Paper>
-					<Grid container direction={"row"} justify={"space-between"} alignItems={"center"}>
+					<Grid
+						container
+						direction={'row'}
+						justify={'space-between'}
+						alignItems={'center'}>
 						<Grid item>
 							<Tooltip title={status}>
-								<DonutSmall style={{color: mapUserStatusColor.get(status)}}
-								            className={classes.statusIcon}/>
+								<DonutSmall
+									style={{ color: mapUserStatusColor.get(status) }}
+									className={classes.statusIcon}
+								/>
 							</Tooltip>
 						</Grid>
 						<Grid item xs>
-							<Typography gutterBottom variant="h4" align={"center"}>{pseudo}</Typography>
+							<Typography gutterBottom variant="h4" align={'center'}>
+								{pseudo}
+							</Typography>
 						</Grid>
 						<Grid item>
 							<Avatar alt={pseudo}
@@ -60,16 +66,19 @@ class ProfileFriend extends React.Component {
 							        className={classes.avatar}/>
 						</Grid>
 					</Grid>
-					<Button variant="contained" color="primary" className={classes.button}>
+					<Button
+						variant="contained"
+						color="primary"
+						className={classes.button}>
 						Compare
-						<CompareArrows className={classes.buttonRightIcon}/>
+						<CompareArrows className={classes.buttonRightIcon} />
 					</Button>
 				</Paper>
-				<StatsTable/>
-				<TableEnigmas/>
+				<StatsTable />
+				<TableEnigmas />
 			</div>
-		);
+		)
 	}
 }
 
-export default withStyles(styles)(ProfileFriend);
+export default withStyles(styles)(ProfileFriend)

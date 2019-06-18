9c00929ddd44be8de9f8cef3f99163d36744d60e
Loïc Colat <loic.colat@gmail.com>
2019-06-18 23:29:47 +0200

    :bug: change donwload cmd

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/enigmator/enigmator-web-app/
-wget https://enigmator-storage.s3.eu-west-3.amazonaws.com/enigmator-web-app/.env
+aws s3 cp s3://enigmator-storage/enigmator-web-app/.env .env
 npm run build
 cd /home/ubuntu/enigmator/
 docker-compose restart

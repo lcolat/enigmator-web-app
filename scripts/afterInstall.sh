dd0189da8a3280ce342efa6d54ff77bbeb41986d
Loïc Colat <loic.colat@gmail.com>
2019-06-18 22:30:27 +0200

    :bug: nasty script

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,4 +1,5 @@
-cd ~/enigmator/enigmator-web-app/
+cd /home/ubuntu/enigmator/enigmator-web-app/
+wget https://enigmator-storage.s3.eu-west-3.amazonaws.com/enigmator-web-app/.env
 npm run build
-cd ~/enigmator/
+cd /home/ubuntu/enigmator/
 docker-compose restart

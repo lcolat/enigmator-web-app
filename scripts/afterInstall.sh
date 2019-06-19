43570a9b3ee0a2f0b7d01b9e06019a899d433240
Loïc Colat <loic.colat@gmail.com>
2019-06-19 10:36:59 +0200

    :construction: change owner and group

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,8 +2,8 @@
 PATH="/home/ubuntu/.local/bin:$PATH"
 cd /home/ubuntu/enigmator/enigmator-web-app/
 cp /home/ubuntu/enigmator/environment-files/enigmator-web-app/.env /home/ubuntu/enigmator/enigmator-web-app/.env
-chown -Rf ubuntu /home/ubuntu/enigmator/enigmator-web-app/
-# npm install
+chown -Rf ubuntu:ubuntu /home/ubuntu/enigmator/enigmator-web-app/
+npm install
 # sudo npm run build
 # cd /home/ubuntu/enigmator/
 # sudo docker-compose restart

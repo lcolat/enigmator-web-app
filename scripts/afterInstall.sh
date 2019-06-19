66346904f649a694d8297a17e987f3c6616261e2
Loïc Colat <loic.colat@gmail.com>
2019-06-19 10:30:36 +0200

    :construction: runas root

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,7 +2,7 @@
 PATH="/home/ubuntu/.local/bin:$PATH"
 cd /home/ubuntu/enigmator/enigmator-web-app/
 cp /home/ubuntu/enigmator/environment-files/enigmator-web-app/.env /home/ubuntu/enigmator/enigmator-web-app/.env
-# sudo npm install
+# npm install
 # sudo npm run build
 # cd /home/ubuntu/enigmator/
 # sudo docker-compose restart

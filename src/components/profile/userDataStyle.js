10db2513feff1df9236b974ac2e9614949cf3bc5
Loïc Colat <loic.colat@gmail.com>
2019-06-19 16:15:30 +0200

    :construction: extract style from component

---
+++ src/components/profile/userDataStyle.js
@@ -0,0 +1,27 @@
+const style = theme => ({
+	container: {
+		display: 'flex',
+		flexWrap: 'wrap'
+	},
+	textField: {
+		marginLeft: theme.spacing(),
+		marginRight: theme.spacing(),
+		minWidth: '152px',
+		maxWidth: '200px'
+	},
+	margin: {
+		margin: 'normal'
+	},
+	dense: {
+		marginTop: 16
+	},
+	menu: {
+		width: 200
+	},
+	avatar: {
+		margin: 10,
+		width: 120,
+		height: 120
+	}
+})
+export default style

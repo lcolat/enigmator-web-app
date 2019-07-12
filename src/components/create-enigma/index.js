ae804508e640057abfa97ddfe4a684d94693037c
Loïc Colat <loic.colat@gmail.com>
2019-07-12 14:34:06 +0200

    :sparkles: add loader on enigma creation

--- src/components/create-enigma/index.js
+++ src/components/create-enigma/index.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid'
 import withStyles from '@material-ui/core/styles/withStyles'
 import { enigmasTypes } from 'model/Enigma'
 import EnigmaBuilder from './enigma-builder'
+import Loader from 'components/loader'
 const style = theme => ({
 	button: {
 		margin: theme.spacing(1),
@@ -20,7 +21,11 @@ const style = theme => ({
 })
 class EnigmaTypeSelection extends Component {
 	state = {
-		type: undefined
+		type: undefined,
+		loaded: true
+	}
+	setLoaded = value => {
+		this.setState({ loaded: value })
 	}
 
 	handleClick = type => {
@@ -32,57 +37,62 @@ class EnigmaTypeSelection extends Component {
 
 	render() {
 		const { classes } = this.props
-		return this.state.type !== undefined ? (
-			<EnigmaBuilder
-				{...this.props}
-				type={this.state.type}
-				setEnigmaType={this.setEnigmaType}
-			/>
-		) : (
-			<Grid
-				container
-				alignItems={'center'}
-				justify={'space-between'}
-				direction={'column'}
-				style={{ justifyContent: 'center' }}
-				className={classes.enigmaType}>
-				<Button
-					className={classes.button}
-					variant="contained"
-					color="secondary"
-					onClick={() => {
-						this.setState({ type: enigmasTypes.IMAGE })
-					}}>
-					Nouvelle énigme Visuelle
-				</Button>
-				<Button
-					className={classes.button}
-					variant="contained"
-					color="secondary"
-					onClick={() => {
-						this.setState({ type: enigmasTypes.TEXT })
-					}}>
-					Nouvelle énigme Textuelle
-				</Button>
-				<Button
-					className={classes.button}
-					variant="contained"
-					color="secondary"
-					onClick={() => {
-						this.setState({ type: enigmasTypes.AUDIO })
-					}}>
-					Nouvelle énigme Audio
-				</Button>
-				<Button
-					className={classes.button}
-					variant="contained"
-					color="secondary"
-					onClick={() => {
-						this.setState({ type: enigmasTypes.VIDEO })
-					}}>
-					Nouvelle énigme Vidéo
-				</Button>
-			</Grid>
+		return (
+			<Loader loaded={this.state.loaded}>
+				{this.state.type !== undefined ? (
+					<EnigmaBuilder
+						{...this.props}
+						type={this.state.type}
+						setEnigmaType={this.setEnigmaType}
+						setLoaded={this.setLoaded}
+					/>
+				) : (
+					<Grid
+						container
+						alignItems={'center'}
+						justify={'space-between'}
+						direction={'column'}
+						style={{ justifyContent: 'center' }}
+						className={classes.enigmaType}>
+						<Button
+							className={classes.button}
+							variant="contained"
+							color="secondary"
+							onClick={() => {
+								this.setState({ type: enigmasTypes.IMAGE })
+							}}>
+							Nouvelle énigme Visuelle
+						</Button>
+						<Button
+							className={classes.button}
+							variant="contained"
+							color="secondary"
+							onClick={() => {
+								this.setState({ type: enigmasTypes.TEXT })
+							}}>
+							Nouvelle énigme Textuelle
+						</Button>
+						<Button
+							className={classes.button}
+							variant="contained"
+							color="secondary"
+							onClick={() => {
+								this.setState({ type: enigmasTypes.AUDIO })
+							}}>
+							Nouvelle énigme Audio
+						</Button>
+						<Button
+							className={classes.button}
+							variant="contained"
+							color="secondary"
+							onClick={() => {
+								this.setState({ type: enigmasTypes.VIDEO })
+							}}>
+							Nouvelle énigme Vidéo
+						</Button>
+					</Grid>
+				)}
+			</Loader>
 		)
 	}
 }
